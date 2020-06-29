085205ae5dea800ebeed99d5268355448ef915b0
Ajay Kumar <ajaykumar201761@gmail.com>
2020-06-29 23:26:02 +0530

    Send.php updated

--- send.php
+++ send.php
@@ -10,57 +10,59 @@ $num=$_POST["num"];
 $subject=$_POST["subject"];
 $message=$_POST["message"];
 
+echo $dates." ".$name." ".$email." ".$num." ".$message;
 
-$servername = "localhost";
-$username = "username";
-$password = "password";
-$dbname = "dbname";
-$con=mysqli_connect($servername, $username, $password, $dbname);
-//$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if (mysqli_connect_errno())
-{
-echo "Failed to connect to MySQL: " . mysqli_connect_error();
-}
 
-$sql = "INSERT INTO ajay_contact (dates, name, email, conatct, subject, message)
-VALUES ('$dates','$name', '$email', '$num', '$subject', '$message')";
-mysqli_query($con,$sql);
+// $servername = "localhost";
+// $username = "username";
+// $password = "password";
+// $dbname = "dbname";
+// $con=mysqli_connect($servername, $username, $password, $dbname);
+// //$conn = new mysqli($servername, $username, $password, $dbname);
+// // Check connection
+// if (mysqli_connect_errno())
+// {
+// echo "Failed to connect to MySQL: " . mysqli_connect_error();
+// }
 
-$msg= <<<MESSAG
-Hi $name,
-Thank you for contacting me.
-I will get to you soon.
+// $sql = "INSERT INTO ajay_contact (dates, name, email, conatct, subject, message)
+// VALUES ('$dates','$name', '$email', '$num', '$subject', '$message')";
+// mysqli_query($con,$sql);
 
-Thanking you
-Ajay Kumar
-MESSAG;
+// $msg= <<<MESSAG
+// Hi $name,
+// Thank you for contacting me.
+// I will get to you soon.
 
-$subjectas="Ajay will contact you soon.";
-$msg2= <<<MESSAG2
-Hi Ajay Kumar,
-  Someone has contacted you with the following details.
-  Date: $dates
-  Name: $name
-  Email-ID: $email
-  Number: $num
-  Subject: $subject
-  Message: $message
+// Thanking you
+// Ajay Kumar
+// MESSAG;
 
-Ajay Kumar
-MESSAG2;
+// $subjectas="Ajay will contact you soon.";
+// $msg2= <<<MESSAG2
+// Hi Ajay Kumar,
+//   Someone has contacted you with the following details.
+//   Date: $dates
+//   Name: $name
+//   Email-ID: $email
+//   Number: $num
+//   Subject: $subject
+//   Message: $message
 
-$subject2="New contact from ".$name;
-$email2="ajaykumarcse.cec@gmail.com";
+// Ajay Kumar
+// MESSAG2;
 
-mail($email, $subjectas, $msg);
-mail($email2, $subject2, $msg2);
+// $subject2="New contact from ".$name;
+// $email2="ajaykumarcse.cec@gmail.com";
 
-echo '<script type="text/javascript">'; 
-echo 'alert("Sent Successfully");'; 
-echo 'window.location.href = "index.html";';
-echo '</script>';
-$conn->close();
+// mail($email, $subjectas, $msg);
+// mail($email2, $subject2, $msg2);
+
+// echo '<script type="text/javascript">'; 
+// echo 'alert("Sent Successfully");'; 
+// echo 'window.location.href = "index.html";';
+// echo '</script>';
+// $conn->close();
 
 ?>
 
